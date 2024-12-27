febb008f8e2158f3dbc48193cbf9ed282ac61d5d
skommana04 <skommana04@gmail.com>
2024-12-27 10:35:56 -0600

    change

--- statefile.tf
+++ statefile.tf
@@ -3,7 +3,7 @@ terraform {
     bucket  = "github-terraform-medha-bucket"
     key     = "medha.tfstate"
     region  = "us-east-1"
-    profile = "default"
+    #profile = "default"
     dynamodb_table = "terraform-remote-state-table"
   }
 }
