d32c1d9635db30d7cba79dfe13b8528aa9a34241
skommana04 <skommana04@gmail.com>
2024-12-27 10:23:43 -0600

    added

---
+++ statefile.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "github-terraform-medha-bucket"
+    key     = "medha.tfstate"
+    region  = "us-east-1"
+    profile = "default"
+    dynamodb_table = "terraform-remote-state-table"
+  }
+}
